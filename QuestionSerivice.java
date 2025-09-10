9a10adc61470dcc26c9a75c1c33e465df68e9ccb
arun-techie <arunaadvik1@gmail.com>
2025-09-10 20:30:04 +0530

    project 1 was pending stage

---
+++ QuestionSerivice.java
@@ -0,0 +1,7 @@
+public class QuestionSerivice {
+    Questions[] questions = new Questions[5];
+
+    public void displayQuestion(){
+        System.out.println(questions[0].getQuestions());
+    }
+}
--- project1.java
+++ project1.java
@@ -1,2 +1,6 @@
 public class project1 {
+    public static void main(String[] args) {
+        QuestionSerivice serivice= new QuestionSerivice();
+        serivice.displayQuestion();
+    }
 }
