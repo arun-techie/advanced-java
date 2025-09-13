584528f41e4356c725c5312791545545dad88a39
arun-techie <arunaadvik1@gmail.com>
2025-09-13 22:57:39 +0530

    lambda expression

--- functionalInterface.java
+++ functionalInterface.java
@@ -15,19 +15,17 @@ public class functionalInterface {
     //functional interface are have just one method
     public static void main(String[] args) {
         //we can define A class show() like
-        A obj=new A() {
-            @Override
-            public void show() {
+        A obj=()-> { //"-> is called lambda expression"this is called syntactically sugar where u reduced the code
                 System.out.println("is a A class");
             }
-        };
+        ;
         obj.show();
         B obj1 = new B();
         obj1.show();
         //java is no more verbose
         //showing extra details or explanations about what’s happening inside the program or tool.
         System.out.println(c.a);
-        //see how i get a value of "a" because it was static method we access static method
-        //into the another static method we dont need to create a object for it
+        //see how I get a value of "a" because it was static method we access static method
+        //into the another static method we don't need to create an object for it
     }
 }
