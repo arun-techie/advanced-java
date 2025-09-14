b3a5d8e55def557dade91cc1678a11e68130e044
arun-techie <arunaadvik1@gmail.com>
2025-09-14 12:56:04 +0530

    exception Handling error

---
+++ exceptionHandling.java
@@ -0,0 +1,10 @@
+public class exceptionHandling {
+    //types of error: compile time error, Runtime error, logical error
+    public static void main(String[] args) {
+        //exception handling is for avoid runtime error
+        //exception handling using try catch
+        int i=0;
+        int j=18/i;
+        System.out.println(j);
+    }
+}
