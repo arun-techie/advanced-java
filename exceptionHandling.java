5e1348f784b69a8ab4457cf819801fc24fbc4b79
arun-techie <arunaadvik1@gmail.com>
2025-09-14 13:20:36 +0530

    exception Handling try and multiple catch

--- exceptionHandling.java
+++ exceptionHandling.java
@@ -3,12 +3,20 @@ public class exceptionHandling {
     public static void main(String[] args) {
         //exception handling is for avoid runtime error
         //exception handling using try catch
-        int i=0;
+        int i=2;
+        int[] nums = new int[5];
+        nums[4]=12;
         try {
             int j = 18 / i;
             System.out.println(j);
-        } catch (Exception e) {
+            System.out.println(nums[4]);
+            System.out.println(nums[4]+j);
+        } catch (ArithmeticException e) {
             System.out.println("something went wrong");
+        }
+        //multiple catch
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("be with ur limit");
         }
         System.out.println("bye...");
     }
