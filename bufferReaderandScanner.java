8d8072b86a4e4bed37d58a2ba1b449753a4fc087
arun-techie <arunaadvik1@gmail.com>
2025-09-15 13:57:32 +0530

    bufferedReader,Scanner method to input

--- bufferReaderandScanner.java
+++ bufferReaderandScanner.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Scanner;
 public class bufferReaderandScanner {
     public static void main(String[] args) throws IOException {
         System.out.println("enter a number");
@@ -9,6 +9,10 @@ public class bufferReaderandScanner {
         BufferedReader obj=new BufferedReader(in);
         int num=Integer.parseInt(obj.readLine());
         System.out.println(num);
-
+        //scanner
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the value of a");
+        int a= input.nextInt();
+        System.out.println(a);
     }
 }
