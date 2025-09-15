dbda6f81f6a6dee65b2e0742305260df4b342987
arun-techie <arunaadvik1@gmail.com>
2025-09-15 13:55:11 +0530

    bufferedReader,InputStreamReader

--- bufferReaderandScanner.java
+++ bufferReaderandScanner.java
@@ -1,14 +1,14 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class bufferReaderandScanner {
-    public static void main(String[] args) {
-        System.out.println("enter the vlaue of a");
-        try {
-            int a = System.in.read();
-            System.out.println(a);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws IOException {
+        System.out.println("enter a number");
+        InputStreamReader in=new InputStreamReader(System.in);
+        BufferedReader obj=new BufferedReader(in);
+        int num=Integer.parseInt(obj.readLine());
+        System.out.println(num);
 
     }
 }
